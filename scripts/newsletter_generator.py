01f71c6f083a67460ae2ad9db833eb53b3212776
SiaSia <fontaine.alexia8@gmail.com>
2025-03-23 15:50:02 +0100

    update date

--- scripts/newsletter_generator.py
+++ scripts/newsletter_generator.py
@@ -969,7 +969,7 @@ def main():
 
     if md_path:
         # Générer une version HTML avec tout le contenu intégré dans un seul fichier
-        newsletter_date = datetime.now().strftime("%d/%m/%Y")
+        newsletter_date = datetime.now().strftime("/%Y/%m/%d")
         html_path = generate_single_file_html(projects, newsletter_date, output_directory, header_image_exists)
         
         if html_path:
